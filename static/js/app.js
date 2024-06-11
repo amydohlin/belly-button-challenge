1b146d0c535a8a53a70764c0dc1b8aff7b272beb
Amy Dohlin <153555607+amydohlin@users.noreply.github.com>
2024-06-11 16:45:16 -0500

    Update app.js

--- static/js/app.js
+++ static/js/app.js
@@ -3,31 +3,25 @@ const BELLIES_URL = "https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/sampl
 
 var globalData = [];
 
-// function getSubjectData(dataList, subjectID) {
-//     let subjectData = dataList.filter(element => element["id"] == subjectID)[0];
-//     // console.log(subjectData); 
-//     return subjectData;
-// }
-
-
+// Create the function to populate the demo box
 function demoBox(subjectNo) {
 
+    // define variables to store the data per a specific subject number
     let metadata = globalData[0].metadata;
     let metadata1 = metadata.filter(element => element["id"] == subjectNo);
 
     console.log(subjectNo);
     console.log("demoBox", metadata1);
 
-    
-
-    d3.select("#sample-metadata")
-        .selectAll("div")
-        .data(metadata1)
+    // use D3 to call into the html file and direct it to the ID where the demo box is stored
+    d3.select("#sample-metadata") // call the html ID
+        .selectAll("div") // select all the divs within the ID
+        .data(metadata1) // grab the selected subject's metadata
         .enter()
-        .append("div")
-        .classed("card-demo", true)
-        .style("font-weight", function (d) { return "bold"})
-        .html(function (d) {
+        .append("div") // add the data to the div
+        .classed("card-demo", true) 
+        .style("font-weight", function (d) { return "bold"}) // add a little formatting (from tutor session)
+        .html(function (d) { // create an anonymous function to feed in the values from the relevant JSON dictionary
             return `<h6>ID: ${d.id}</h6>
                     <h6>Ethnicity: ${d.ethnicity}</h6>
                     <h6>Gender: ${d.gender}</h6>
@@ -40,7 +34,7 @@ function demoBox(subjectNo) {
 
 }
 
-
+// Create the function to populate the bar chart
 function barChart(subjectNo) {
 
     let samples = globalData[0].samples;
@@ -51,7 +45,7 @@ function barChart(subjectNo) {
     console.log(subjectNo);
     console.log("bar", samples1);
 
-    // select just the first 10 items from the bacteria array. use slice
+    // select just the first 10 items from the bacteria array using slice
     let labels1 = samples1['otu_labels'].slice(0,10);
     let ids1 = samples1['otu_ids'].slice(0,10);
     let sliceSample1 = samples1['sample_values'].slice(0,10);
@@ -59,14 +53,12 @@ function barChart(subjectNo) {
     // create the label for the bacteria id as "OTU + [id number]"
     let ids2 = ids1.map(x => "OTU " + x);
 
-    console.log("update otu id", ids2);
-
-    
+    console.log("update otu id", ids2);  
     console.log('labels sliced',labels1);
     console.log('ids sliced', ids1);
     console.log('values sliced', sliceSample1);
 
-
+    // define the chart type and input data
     let barChart = [{
         x: sliceSample1,
         y: ids2,
@@ -74,6 +66,7 @@ function barChart(subjectNo) {
         orientation: "h"
       }];
     
+    // define the chart layout
     let layout = {
         title: "Top 10 OTUs",
         xaxis: {"title": "Number of Bacteria"},
@@ -82,12 +75,11 @@ function barChart(subjectNo) {
         width: 800
     }
     
-
       Plotly.newPlot("bar", barChart, layout);
 
 }
 
-
+// Create the function to populate the bubble chart
 function bubblePlot(subjectNo) {
 
     let samples = globalData[0].samples;
@@ -95,11 +87,10 @@ function bubblePlot(subjectNo) {
     // filter data to only the chosen id
     let samples1 = samples.filter(element => element["id"] == subjectNo)[0];
   
-
     console.log(subjectNo);
     console.log("bubble", samples1);
 
-    // use samples1 as the subject, then use . notation to call the specific dictionary keys
+    // use samples1 as the subject, then use dot notation to call the specific dictionary keys
     var bubble = [{
         x: samples1.otu_ids,
         y: samples1.sample_values,
@@ -112,6 +103,7 @@ function bubblePlot(subjectNo) {
         text: samples1.otu_labels,
       }];
 
+    // define the chart layout
     let layout = {
         title: "Bacteria Cultures Per Sample",
         xaxis: {'title': "OTU ID"},
@@ -125,9 +117,11 @@ function bubblePlot(subjectNo) {
 
 }
 
+// Create the function to populate the dropdown menu and select subjects
 function optionChanged(subjectNo) {
     console.log(subjectNo);
    
+    // remove the data from the previous subject so the data from the next subject can populate the demo box and charts
     d3.select(".card-demo").remove();
 
     demoBox(subjectNo);
@@ -137,7 +131,7 @@ function optionChanged(subjectNo) {
     
 }
 
-
+// Create the init function to execute all the above functions
 function init(data){
 
     console.log("init function")
